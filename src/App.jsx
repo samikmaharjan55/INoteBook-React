c30106407e6c39f0a0b9a5410f59841cc9105c76
samikmaharjan55 <samikmaharjan55@gmail.com>
2024-07-16 14:07:40 +0545

    Part 56

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar />
+
+        <Routes>
+          <Route exact path="/" element={<Home />}></Route>
+        </Routes>
+        <Routes>
+          <Route exact path="/about" element={<About />}></Route>
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
