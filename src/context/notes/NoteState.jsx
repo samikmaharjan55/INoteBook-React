7f8af88d5372c8e13b06d5aceefc4c8013c75f42
samikmaharjan55 <samikmaharjan55@gmail.com>
2024-07-17 16:53:19 +0545

    Part 59 Ongoing

--- src/context/notes/NoteState.jsx
+++ src/context/notes/NoteState.jsx
@@ -22,3 +22,40 @@ const NoteState = (props) => {
 };
 
 export default NoteState;
+
+// import React from "react";
+// import NoteContext from "./NoteContext";
+// import { useState } from "react";
+
+// const NoteState = (props) => {
+//   const notesInitial = [
+//     {
+//       _id: "asfghlkqwoicnaslkdjwoi",
+//       user: "asdczxbjqwjinzxcuinwq",
+//       title: "My Title",
+//       description: "Please wake up early",
+//       tag: "Personal",
+//       date: "2024-07-17",
+//       _v: "0",
+//     },
+//     {
+//       _id: "123dfacascwasf",
+//       user: "5462343fsdvevsdfe",
+//       title: "My Title",
+//       description: "Please wake up early",
+//       tag: "Personal",
+//       date: "2024-07-17",
+//       _v: "0",
+//     },
+//   ];
+//   const [notes, setNotes] = useState(notesInitial);
+
+//   return (
+//     <NoteContext.Provider value={{ notes, setNotes }}>
+//       {props.children}
+//     </NoteContext.Provider>
+//   );
+// };
+
+// export default NoteState;
+
